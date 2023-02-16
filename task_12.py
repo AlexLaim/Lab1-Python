6d6066b956e0307ae26f23837db5c2fe93454a88
Alex <92443002+AlexLaim@users.noreply.github.com>
2023-02-16 11:37:16 +0300

    Delete task_12.py

--- task_12.py
+++
@@ -1,8 +0,0 @@
-def get_frames(signal, size, overlap):
-    for i in range(0, len(signal), int(size * overlap)):
-        yield signal[i:i + size]
-
-signal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-for frame in get_frames(signal, size=7, overlap=0.5):
-    print(frame)
