598bd9af2bdd932dd842500e914df6ae9837cb7b
Alex <92443002+AlexLaim@users.noreply.github.com>
2023-02-16 11:35:22 +0300

    Delete task_1.py

--- task_1.py
+++
@@ -1,7 +0,0 @@
-sum = float(input('Введите сумму\n'))
-if sum < 0:
-    print("Некорректный формат!"),
-else:
-    def dur(rub): return '{} руб. {:02d} коп.'.format(
-        int(rub // 1), int(rub % 1 * 100))
-    print(dur(sum))
