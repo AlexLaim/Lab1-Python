11a560e096cd2337f23048511ea5e857eb976ac6
Alex <92443002+AlexLaim@users.noreply.github.com>
2023-02-16 11:36:01 +0300

    Delete task_13.py

--- task_13.py
+++
@@ -1,9 +0,0 @@
-def extra_ennymerate(x):
-    frac, cum = 0, 0
-    for i in range(len(x)):
-        cum += x[i]
-        frac = cum/sum(x)
-        yield 0, x[i] , cum , frac
-x = [1,3,4,2]
-for i, elem, cum, frac in extra_ennymerate(x):
-    print(elem, cum, frac)
