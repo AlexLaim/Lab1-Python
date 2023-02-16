8daf13322e75409007aba5c54e1353402ad33e8a
Alex <92443002+AlexLaim@users.noreply.github.com>
2023-02-16 11:37:02 +0300

    Delete task_2.py

--- task_2.py
+++
@@ -1,8 +0,0 @@
-lst = [2, 3, 5, 20, 100]
-
-for i in range(len(lst)):
-    if i != (len(lst) - 1) and lst[i] > lst[i+1]:
-        print("False")
-        break
-    elif i == (len(lst) - 1):
-        print("True")
