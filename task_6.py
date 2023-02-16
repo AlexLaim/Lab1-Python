5c12657a9e7d06bf5bff08fe24873dab19342ab5
Alex <92443002+AlexLaim@users.noreply.github.com>
2023-02-16 11:36:54 +0300

    Delete task_6.py

--- task_6.py
+++
@@ -1,9 +0,0 @@
-words = (
-    input("\nВведите предложение, из которого будут выведены все символы по 1 разу на консоль:\n")
-    .replace(",", " ")
-    .split()
-)
-print("Символы в вашем предложении:")
-for i in range(len(words)):
-    for j in range(len(words[i])):
-        print(words[i][j])
